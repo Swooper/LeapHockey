176bcab70eabed634cba8c614752a78e1d34af50
binni11 <bro3@hi.is>
2015-12-03 13:28:22 +0000

    Some Folders and two empty scripts
    Same as above

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	var movedownY = 0.0;
+	var sensitivityY = 1;
+	// Update is called once per frame
+	void Update () {
+
+	movedownY += Input.GetAxis("Mouse Y") * sensitivityY;
+	if (Input.GetAxis("Mouse Y") != 0){
+		transform.Translate(Vector3.forward * movedownY);
+	}
+	movedownY = 0.0;
+	
+
+
+
+
+
+
+
+		/*float inputSpeed = Input.GetAxisRaw ("HumanPaddle");
+		
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.position += Vector3.forward * Time.deltaTime;
+			
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.position += Vector3.back * Time.deltaTime;
+			
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * Time.deltaTime;
+			
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * Time.deltaTime;
+			
+		}*/
+
+	}
+}
