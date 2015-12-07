2b5fe25d0eb6cb046328b9a1f510e3e98849471b
binni11 <bro3@hi.is>
2015-12-07 14:17:21 +0000

    Added microphone echo

---
+++ Assets/Scripts/Microphone.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Microphone : MonoBehaviour {
+	
+	//[RequireComponent(typeof(AudioSource))]
+	
+	public AudioClip c;
+	
+	void Start() {
+		foreach (string device in Microphone.devices) {
+			Debug.Log("Name: " + device);
+		}
+	}
+	
+	/*void Start() {
+		//AudioClip Micinput= Microphone.Start (null, true, 10, 44100);
+
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		aud.Play();
+	}*/
+	public void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.T)) {
+			
+			if(Microphone.IsRecording("Built-in Microphone")){
+				StopRecording();
+			}
+			
+			else{
+				StartRecording();
+			}
+		}
+		
+		
+	}
+	void StartRecording()
+	{
+		c = Microphone.Start ("Built-in Microphone",true, 300, 44100);
+		while (!(Microphone.GetPosition("Built-in Microphone")>0)) {
+		}
+		GetComponent<AudioSource> ().PlayOneShot (c);
+		
+		
+	}
+	
+	void StopRecording()
+	{
+		Microphone.End ("Built-in Microphone");
+		GetComponent<AudioSource> ().Stop ();
+		
+	}
+	
+}
