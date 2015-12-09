f465599346368ed38a681b194500403c9ca4a4ed
Haukur Jónasson <bluecloak+assembla@gmail.com>
2015-12-09 22:25:51 +0000

    ScoreManager should now be resetting positions on score. For some reason puck won't move atm, so can't test it.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -9,6 +9,17 @@ public class ScoreManager : MonoBehaviour {
 
 	private HUD hud;
 
+	private GameObject player;
+	private GameObject enemy;
+	private GameObject puck;
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
+	public GameObject puckPrefab;
+	private GameObject playerStartPos;
+	private GameObject playerPuckStart;
+	private GameObject enemyStartPos;
+	private GameObject enemyPuckStart;
+
 	// Use this for initialization
 	void Start () {
 		playerScore = 0;
@@ -16,6 +27,17 @@ public class ScoreManager : MonoBehaviour {
 		winScore = 5;
 
 		hud = GameObject.Find("GUICanvas").GetComponent<HUD>();
+
+		playerStartPos = GameObject.Find("PlayerStartPos");
+		enemyStartPos = GameObject.Find("EnemyStartPos");
+		playerPuckStart = GameObject.Find("PlayerPuckStart");
+		enemyPuckStart = GameObject.Find("EnemyPuckStart");
+
+		puck = GameObject.Instantiate(puckPrefab);
+		player = GameObject.Instantiate(playerPrefab);
+		enemy = GameObject.Instantiate(enemyPrefab);
+
+		ResetPositions(true);
 	}
 
 	public void AddScore(bool player) {
@@ -32,5 +54,19 @@ public class ScoreManager : MonoBehaviour {
 			}
 		}
 		hud.UpdateScore(playerScore, enemyScore);
+		ResetPositions(player);
+	}
+
+	// playerSide true if the puck should spawn on the player's side,
+	// false if it should be on the other side
+	private void ResetPositions(bool playerSide) {
+		player.transform.position = playerStartPos.transform.position;
+		enemy.transform.position = enemyStartPos.transform.position;
+		if(playerSide) {
+			puck.transform.position = playerPuckStart.transform.position;
+		}
+		else {
+			puck.transform.position = enemyPuckStart.transform.position;
+		}
 	}
 }
