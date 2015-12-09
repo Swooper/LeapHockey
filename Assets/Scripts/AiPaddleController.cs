7238c3bdf67cf3cb82c1aa8a77f89ecd8184cdbe
binni11 <bro3@hi.is>
2015-12-09 22:42:03 +0000

    bilað stuff

--- Assets/Scripts/AiPaddleController.cs
+++ Assets/Scripts/AiPaddleController.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 
 public class AiPaddleController : MonoBehaviour {
-
+	public float speed = 15f;
+	//private Rigidbody body;
+	public float puckXPos;
+	public float puckZPos;
 	// Use this for initialization
 	void Start () {
-	
+		 puckXPos = GameObject.Find ("Puck").transform.position.x;
+		 puckZPos = GameObject.Find ("Puck").transform.position.z;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		float puckXPos = GameObject.Find ("Puck").transform.position.x;
-		float puckZPos = GameObject.Find ("Puck").transform.position.z;
+
 		float opponentXPos = transform.position.x;
+		float opponentZPos = transform.position.z;
+
 		if (puckXPos != opponentXPos) {
 			if (opponentXPos > puckXPos)
 				transform.position += Vector3.left * Time.deltaTime;
@@ -23,7 +28,11 @@ public class AiPaddleController : MonoBehaviour {
 			}
 		}
 		if (puckZPos > 0) {
-			transform.position += Vector3.back * Time.deltaTime;
+			//Vector3 movement = new Vector3(0.0f, 0.0f, -1f);
+
+			//body.velocity = movement * speed;
+			transform.position += Vector3.back * speed * Time.deltaTime;
+			//transform.position += Vector3.back * Time.deltaTime;
 		}
 
 //		Debug.Log ("xpos :" + opponentXPos);
